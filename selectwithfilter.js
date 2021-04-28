5264fd53d22abaa2d5d5d7716735abec64f1d65a
talanaji <talanaji@gmail.com>
2021-04-29 00:50:12 +0200

    Select with escaping query values

--- selectwithfilter.js
+++ selectwithfilter.js
@@ -8,13 +8,27 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  /* con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });*/
-  con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
+	if (err) throw err;
+	/* con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+	if (err) throw err;
+	console.log(result);
+	});*/
+	con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
+	if (err) throw err;
+	console.log(result);
+	});
+
+///When query values are variables provided by the user, you should escape the values or ? placeholder
+	var adr = 'Mountain 21';
+	var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+	con.query(sql, function (err, result) {
+	  if (err) throw err;
+	  console.log(result);
+	});
+
+	var sql = 'SELECT * FROM customers WHERE address = ?';
+	con.query(sql, [adr], function (err, result) {
+	  if (err) throw err;
+	  console.log(result);
+	});
 });
