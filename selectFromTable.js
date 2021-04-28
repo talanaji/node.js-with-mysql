952e97ae5b6b7eb67232396fc5aa00200958d6ac
talanaji <talanaji@gmail.com>
2021-04-29 00:34:17 +0200

    Select columns from  Table return fields object

--- selectFromTable.js
+++ selectFromTable.js
@@ -9,9 +9,15 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT name, address FROM customers", function (err, result, fields) {
+  /*con.query("SELECT name, address FROM customers", function (err, result, fields) {
     if (err) throw err;
     //console.log(result);
 	console.log(result[0].address);
+  });*/
+  //The third parameter of the callback function is an array containing information about each field in the result.
+   con.query("SELECT name, address FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(fields);
+    console.log(fields[1].name);
   });
 });
