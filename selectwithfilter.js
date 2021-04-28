15007f881c1de62ab5fb86b996563c80f763b221
talanaji <talanaji@gmail.com>
2021-04-29 00:39:39 +0200

    Select with filter

---
+++ selectwithfilter.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  /* con.query("SELECT * FROM customers WHERE address = 'Park Lane 38'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });*/
+  con.query("SELECT * FROM customers WHERE address LIKE 'S%'", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
